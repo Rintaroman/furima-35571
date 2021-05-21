b5987f0d89bc72d8fd6c45b5cd625ecea02dc91c
Rintaroman <rintaro.584@gmail.com>
2021-05-21 17:43:54 +0900

    商品情報編集機能　修正②

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,7 +1,7 @@
 class FurimasController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update] #:delete]
   before_action :set_furima, only: [:show, :edit, :update]
   before_action :set_edit, only: [:edit, :update]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update] #:delete]
   
   def index
     @furimas = Furima.order("created_at DESC")
